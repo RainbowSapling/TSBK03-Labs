eea50c0c1623c49ad15435e0ec11e8f0222b1014
RainbowSapling <mirbj463@student.liu.se>
2025-10-01 17:27:36 +0200

    Lab4 setup

---
+++ Lab4/lab4/SpriteLight2.h
@@ -0,0 +1,49 @@
+#ifndef _SPRITELIGHT_
+#define _SPRITELIGHT_
+
+// SpriteLight - Heavily simplified sprite engine
+// by Ingemar Ragnemalm 2009
+
+// What does a mogwai say when it sees a can of soda?
+// Eeek! Sprite light! Sprite light!
+
+#include "LoadTGA.h"
+#include "VectorUtils4.h"
+#include <vector>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SpriteRec
+{
+	vec3 position; // OBS! Really 2D, declared as vec3 to make functions like normalizations compatible.
+	TextureData *face;
+	vec3 speed;
+	GLfloat rotation;
+	struct SpriteRec *next;
+	
+	// Add custom sprite data here as needed
+} SpriteRec, *SpritePtr;
+
+// Globals: The sprite list, background texture and viewport dimensions (virtual or real pixels)
+//extern SpritePtr gSpriteRoot;
+extern std::vector<SpriteRec> sprites;
+extern GLuint backgroundTexID;
+extern long gWidth, gHeight;
+
+// Functions
+TextureData *GetFace(const char *fileName);
+int NewSprite(TextureData *f, GLfloat h, GLfloat v, GLfloat hs, GLfloat vs);
+void HandleSprite(SpritePtr sp);
+void DrawSprite(SpritePtr sp);
+void DrawBackground();
+
+void InitSpriteLight();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
